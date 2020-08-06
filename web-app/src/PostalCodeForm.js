b649fd6387de38ac4d8497ddde5681af02eb8ed3
Stéphane TOUBIANA <stephane.toubiana@kisio.com>
2020-08-06 21:03:35 +0200

    [FEATURE]: Search history is saved in and read from localstorage

--- web-app/src/PostalCodeForm.js
+++ web-app/src/PostalCodeForm.js
@@ -1,6 +1,8 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
-import { Form } from 'semantic-ui-react';
+import { Form, Dropdown } from 'semantic-ui-react';
+
+import localStorageHelper from './localstorage-helper';
 
 class PostalCodeForm extends Component {
   constructor(props) {
@@ -17,11 +19,24 @@ class PostalCodeForm extends Component {
   render() {
     const { loading, postalCodeSearchValue, onChangedPostalCode } = this.props;
 
+    const searchHistory = localStorageHelper.readItem('searchHistory');
+    const dropdownOptions = searchHistory.map((element, i) => ({key: i, text: element, value: element}))
+
     return (
       <Form onSubmit={this.handleSubmit}>
         <Form.Input
           fluid
           focus
+          action={
+            <Dropdown
+              button
+              basic
+              floating
+              onChange={onChangedPostalCode}
+              options={dropdownOptions}
+              defaultValue=""
+            />
+          }
           onChange={onChangedPostalCode}
           placeholder="Search Location by Postal Code…"
           value={postalCodeSearchValue}
