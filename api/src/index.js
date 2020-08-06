d6da00aa48e4a2284122681408b456ea94c97773
Stéphane TOUBIANA <stephane.toubiana@kisio.com>
2020-08-06 15:45:41 +0200

    [FEATURE]: 0002 - Rate limit the API

--- api/src/index.js
+++ api/src/index.js
@@ -1,6 +1,7 @@
 import compression from 'compression';
 import cors from 'cors';
 import express from 'express';
+import rateLimit from "express-rate-limit";
 import logger from './lib/logger';
 import handleError from './middlewares/handle-error';
 import logQuery from './middlewares/log-query';
@@ -9,11 +10,18 @@ import locationsRouter from './routes/locations';
 
 const port = process.env.PORT || 4000;
 
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100 // limit each IP to 100 requests per windowMs
+});
+
 process.on('unhandledRejection', (reason, promise) => {
   logger.error('Unhandled Rejection at: Promise ', promise, reason);
 });
 
 const app = express();
+
+app.use('*', limiter);
 
 app.use(compression());
 app.use(cors());
